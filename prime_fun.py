a8240fb603b31ef96fd0363aac0a1f1beb94e5fd
Shashi Sriramula <71032166+ShashiKumar-2019@users.noreply.github.com>
2022-05-18 15:39:00 +0530

    Update prime_fun.py

--- prime_fun.py
+++ prime_fun.py
@@ -2,7 +2,7 @@ def Prime(num):
     index=2
     while index<num:
         if num%index==0:
-            return str(num)+" not a prime"
+            return str(num)+" not a prime number"
             break
         index+=1
     else:
