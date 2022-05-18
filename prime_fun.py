30bd27293768acd59703e20363268e29620cae44
Shashi Sriramula <71032166+ShashiKumar-2019@users.noreply.github.com>
2022-05-18 15:29:47 +0530

    Add files via upload

---
+++ prime_fun.py
@@ -0,0 +1,11 @@
+def Prime(num):
+    index=2
+    while index<num:
+        if num%index==0:
+            return str(num)+" not a prime"
+            break
+        index+=1
+    else:
+        return str(num)+" is a prime"
+p=Prime(int(input()))
+print(p)
